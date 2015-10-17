5d5e79212bbe405dd1ba6cb55a3f748d3293ac96
hazyumps <psh.aston@gmail.com>
2015-10-17 10:56:58 -0500

    From Desktop

--- HangoutsPuller/Program.cs
+++ HangoutsPuller/Program.cs
@@ -8,15 +8,14 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-//using System.Threading;
 using System.Threading.Tasks;
 
-namespace GmailQuickstart
+namespace HangoutPuller
 {
     class Program
     {
         static string[] Scopes = { GmailService.Scope.GmailReadonly };
-        static string ApplicationName = "Gmail API .NET Quickstart";
+        static string ApplicationName = "HangoutPuller";
 
         static void Main(string[] args)
         {
@@ -44,13 +43,83 @@ namespace GmailQuickstart
                 HttpClientInitializer = credential,
                 ApplicationName = ApplicationName,
             });
-            string userId = "me";
-            string threadId = "14ea6f66f112fc79";
-            //lsThr(service, userId);
 
-            Console.WriteLine(GetThread(service,userId,threadId).Messages);
+            //Define inputs 
+            //'me' is special for being current logged in user
+            //
+            //Using the labelId of CHAT we pull all hangouts history
+            //query will need to be modified in the future to be dynamic based on the Sharepoint site
+            string userId = "me";
+            string query = "from:ben.s.lerch@gmail.com ";
+            string labelId = "CHAT";
+
+            //Call the ListThread method to pull all threadIds based on the criteria listed
+            List<Thread> t = ListThread(service, userId, labelId, query);
+
+            //Enumerate through the list
+            foreach (var x in t)
+            {
+                //Call the GetThread method to pull actual messages based on the threadIds found in the previous list
+                Thread th = GetThread(service, userId, x.Id);
+
+                //Enumerate the message contents
+                foreach (var y in th.Messages)
+                {
+                    //Search the headers for the name of the sender and highlight it in RED so I can see that shit
+                    foreach (var z in y.Payload.Headers)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(z.Value);
+                    }
+
+                    //Write out the chat to the console window!
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(y.Snippet);
+                }
+
+            }
+
+
+            Console.ReadLine();
         }
 
+        public static List<Message> ListMessages(GmailService service, String userId, String query)
+        {
+            List<Message> result = new List<Message>();
+            UsersResource.MessagesResource.ListRequest request = service.Users.Messages.List(userId);
+            request.Q = query;
+
+            do
+            {
+                try
+                {
+                    ListMessagesResponse response = request.Execute();
+                    result.AddRange(response.Messages);
+                    request.PageToken = response.NextPageToken;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occurred: " + e.Message);
+                }
+
+            } while (!String.IsNullOrEmpty(request.PageToken));
+
+            return result;
+        }
+
+        public static Message GetMessage(GmailService service, String userId, String messageId)
+        {
+            try
+            {
+                return service.Users.Messages.Get(userId, messageId).Execute();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: " + e.Message);
+            }
+
+            return null;
+        }
         public static Thread GetThread(GmailService service, String userId, String threadId)
         {
             try
@@ -67,25 +136,20 @@ namespace GmailQuickstart
         }
 
 
-        public static List<Thread> lsThr(GmailService service, String userId)
+        public static List<Thread> ListThread(GmailService service, String userId, String labelIds, String query)
         {
-            List<Thread> result = new List <Thread>();
+            List<Thread> result = new List<Thread>();
             UsersResource.ThreadsResource.ListRequest request = service.Users.Threads.List(userId);
+            request.LabelIds = labelIds;
+            request.Q = query;
 
             do
             {
                 try
                 {
+
                     ListThreadsResponse response = request.Execute();
                     result.AddRange(response.Threads);
-                    if (response.Threads != null)
-                    {
-                        foreach (Thread t in response.Threads)
-                        {
-                            //GetThread(service, userId, t.Id);
-                            Console.WriteLine(t.Id);
-                        }
-                    }
                     request.PageToken = response.NextPageToken;
 
 
@@ -96,11 +160,11 @@ namespace GmailQuickstart
                 }
 
             } while (!String.IsNullOrEmpty(request.PageToken));
-            
+
             return result;
         }
 
-       
+
     }
 
 }
